69654cc46390247c674947c366ce10c52e34028e
Marcos Eleno Joaquín Simental Magaña <mrkzmrkz@gmail.com>
2013-08-28 19:44:50 -0500

    Comienzo codigo de practica 2.

---
+++ Practica 2/src/Conversor.java
@@ -0,0 +1,11 @@
+/**
+ * Taller de Sistemas Operativos Avanzados - 2013B
+ * Simental Magaña Marcos Eleno Joaquín.
+ * Sección D04 - Tarea 2
+ */
+public class Conversor {
+ static void main(String[] args) {
+
+	}
+
+}
